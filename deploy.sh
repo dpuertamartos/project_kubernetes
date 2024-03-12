cddc8a108d8981d5bfe0df19b889f86978c10897
David Puerta Martos <dpuerta@usal.es>
2024-03-12 11:09:52 +0100

    added secret for PORT

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,8 @@ chmod +x ./frontend/build.sh
 ./backend/build.sh
 ./frontend/build.sh
 
+SOPS_AGE_KEY_FILE=$(pwd)/key.txt
+sops --decrypt ./manifests/secret.enc.yaml > ./manifests/secret.yaml
 kubectl delete -f ./manifests 
 kubectl apply -f ./manifests
+rm ./manifests/secret.yaml
