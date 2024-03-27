e210c1b155a2342a4aa286dc2b579a8dfa1e3f1f
David Puerta Martos <dpuerta@usal.es>
2024-03-27 19:55:13 +0100

    added cronjob of exercise 2.09

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,11 @@ kubectl create namespace project
 
 chmod +x ./backend/build.sh
 chmod +x ./frontend/build.sh
+chmod +x ./cronjob/build.sh
 
 ./backend/build.sh
 ./frontend/build.sh
+./cronjob/build.sh
 
 export SOPS_AGE_KEY_FILE=$(pwd)/key.txt
 sops --decrypt ./manifests/secret.enc.yaml > ./manifests/secret.yaml
