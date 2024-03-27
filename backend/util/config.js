0b737896765e690f27380a4d3f160b82c1b0810d
David Puerta Martos <dpuerta@usal.es>
2024-03-27 18:39:22 +0100

    postgres password not retrieved from secret.enc.yaml

--- backend/util/config.js
+++ backend/util/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
 
 module.exports = {
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres:1234@postgres-project-svc:5432/postgres',
+    DATABASE_URL: process.env.DATABASE_URL || `postgresql://postgres:${process.env.POSTGRES_PASSWORD}@postgres-project-svc:5432/postgres`,
 }
